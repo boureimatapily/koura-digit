f7f3d64905a47819dab4cf3d19af5e5fb0793cbb
unknown <boureimatapily1@gmail.com>
2020-10-27 16:00:14 +0000

    MOD add design

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, View, StyleSheet, Image} from 'react-native';
+import { Button, View, StyleSheet, Image, Text, TouchableOpacity} from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 import Constants from 'expo-constants';
 
@@ -8,12 +8,16 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.bnvContainer}>
+        <Text  style={styles.bnv}>Bienvenue sur Koura Digit</Text>
+        <Text>le Meilleur site de vente</Text>
+        </View>
+        
         <Image source={require('./assets/logo.png')}  style={styles.image}/>
-        <Button
-          title="Commencer"
-          onPress={this._handleOpenWithWebBrowser}
-          style={styles.button}
-        />
+       
+        <TouchableOpacity style={styles.btn}  onPress={this._handleOpenWithWebBrowser}>
+          <Text style={styles.btntext}>Commencer</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -38,5 +42,26 @@ const styles = StyleSheet.create({
   image:{
     height:350,
     width:300
+  },
+  bnv:{
+    fontSize:20
+  },
+  btn:{
+    backgroundColor:"green",
+    height:60,
+    width:200,
+    borderRadius:20,
+    alignItems:"center",
+    paddingTop:15,
+    marginTop:10
+  },
+  btntext:{
+    fontSize:20,
+    color:"white",
+    fontWeight:"bold"
+  },
+  bnvContainer:{
+    marginBottom:20,
+    alignItems:"center"
   }
 });
