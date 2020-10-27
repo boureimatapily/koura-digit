c765029d9f4be7043578b6f5f2f26ff51bf8f48d
unknown <boureimatapily1@gmail.com>
2020-10-27 11:18:50 +0000

    MOD content

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
-import { Button, View, StyleSheet, Image} from 'react-native';
-import * as WebBrowser from 'expo-web-browser';
-import Constants from 'expo-constants';
+// import { Button, View, StyleSheet, Image} from 'react-native';
+// import * as WebBrowser from 'expo-web-browser';
+// import Constants from 'expo-constants';
+import { WebView } from 'react-native-webview';
+
 
 export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Image source={require('./assets/logo.png')}  style={styles.image}/>
-        <Button
-          title="Commencer"
-          onPress={this._handleOpenWithWebBrowser}
-          style={styles.button}
-        />
-      </View>
-    );
+    return <WebView source={{ uri: 'https://koura-digit.com' }} style={{ marginTop: 20 }} />;
   }
 
   _handleOpenWithWebBrowser = () => {
@@ -22,20 +15,20 @@ export default class App extends Component {
   };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-  },
-  button: {
-    marginVertical: 10,
-    width:100
-  },
-  image:{
-    height:350,
-    width:300
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     paddingTop: Constants.statusBarHeight,
+//     backgroundColor: '#ecf0f1',
+//   },
+//   button: {
+//     marginVertical: 10,
+//     width:100
+//   },
+//   image:{
+//     height:350,
+//     width:300
+//   }
+// });
